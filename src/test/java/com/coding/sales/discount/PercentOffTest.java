bbdbfcdb7b31df0fd6acaa9d5cadd84dffe2e084
zhmxcn <zhmx1212@163.com>
2019-07-02 22:30:54 +0800

    优惠活动测试用例新增

--- src/test/java/com/coding/sales/discount/PercentOffTest.java
+++ src/test/java/com/coding/sales/discount/PercentOffTest.java
@@ -11,7 +11,7 @@ import static org.junit.Assert.*;
 public class PercentOffTest {
 
     @Test
-    public void should_20_when_95_off() {
+    public void should_20_when_buy400_95_off() {
         DiscountCard discountCard = new DiscountCard("9.5折", new BigDecimal(0.95));
         Discount discount = new PercentOff(discountCard);
         OrderItem orderItem = new OrderItem();
@@ -40,6 +40,32 @@ public class PercentOffTest {
         orderItem.addProduct(product2);
         orderItem.addProduct(product3);
         assertEquals(new BigDecimal(20).setScale(2, BigDecimal.ROUND_HALF_UP), discount.discount(orderItem));
+    }
+    public void should_0_when_buy400_not_95_off() {
+        DiscountCard discountCard = new DiscountCard("9.5折", new BigDecimal(0.95));
+        Discount discount = new PercentOff(discountCard);
+        OrderItem orderItem = new OrderItem();
+        Product product = new Product();
+        product.setProductNo("0001");
+        product.setProductName("产品1");
+        product.setPrice(new BigDecimal(100.00));
+        Product product1 = new Product();
+        product1.setProductNo("0001");
+        product1.setProductName("产品1");
+        product1.setPrice(new BigDecimal(100.00));
+        Product product2 = new Product();
+        product2.setProductNo("0001");
+        product2.setProductName("产品1");
+        product2.setPrice(new BigDecimal(100.00));
+        Product product3 = new Product();
+        product3.setProductNo("0001");
+        product3.setProductName("产品1");
+        product3.setPrice(new BigDecimal(100.00));
+        orderItem.addProduct(product);
+        orderItem.addProduct(product1);
+        orderItem.addProduct(product2);
+        orderItem.addProduct(product3);
+        assertEquals(new BigDecimal(0).setScale(2, BigDecimal.ROUND_HALF_UP), discount.discount(orderItem));
     }
 
 }

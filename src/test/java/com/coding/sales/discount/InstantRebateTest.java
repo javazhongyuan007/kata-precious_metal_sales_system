0f2f6805a0ee8663714fab27ed2144b500d69b9c
zzh900603 <zhouzh2010@126.com>
2019-07-02 21:25:56 +0800

    满减测试

---
+++ src/test/java/com/coding/sales/discount/InstantRebateTest.java
@@ -0,0 +1,44 @@
+package com.coding.sales.discount;
+
+import com.coding.sales.order.OrderItem;
+import com.coding.sales.product.Product;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.Assert.*;
+
+public class InstantRebateTest {
+
+    @Test
+    public void should_return_350_when_full_3000() {
+        Discount discount = new InstantRebate();
+        OrderItem orderItem = new OrderItem();
+        Product product = new Product();
+        product.setProductNo("0001");
+        product.setProductName("产品1");
+        product.setPrice(new BigDecimal(1000.78));
+        product.getDiscounts().add(discount);
+        Product product1 = new Product();
+        product1.setProductNo("0001");
+        product1.setProductName("产品1");
+        product1.setPrice(new BigDecimal(1000.78));
+        product1.getDiscounts().add(discount);
+        Product product2 = new Product();
+        product2.setProductNo("0001");
+        product2.setProductName("产品1");
+        product2.setPrice(new BigDecimal(1000.78));
+        product2.getDiscounts().add(discount);
+        Product product3 = new Product();
+        product3.setProductNo("0001");
+        product3.setProductName("产品1");
+        product3.setPrice(new BigDecimal(1000.78));
+        product3.getDiscounts().add(discount);
+        orderItem.addProduct(product);
+        orderItem.addProduct(product1);
+        orderItem.addProduct(product2);
+        orderItem.addProduct(product3);
+        assertEquals(new BigDecimal(350.00).setScale(2, BigDecimal.ROUND_HALF_UP), discount.discount(orderItem));
+    }
+
+}

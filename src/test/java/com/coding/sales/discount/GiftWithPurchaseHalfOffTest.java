64ba73a4904dff67b3bf9e5d195c9154e6d972ec
zhmxcn <zhmx1212@163.com>
2019-07-02 22:14:18 +0800

    增加满减测试用例

--- src/test/java/com/coding/sales/discount/GiftWithPurchaseHalfOffTest.java
+++ src/test/java/com/coding/sales/discount/GiftWithPurchaseHalfOffTest.java
@@ -35,4 +35,53 @@ public class GiftWithPurchaseHalfOffTest {
         assertEquals((new BigDecimal(100.78)).divide(new BigDecimal(2)).setScale(2,  BigDecimal.ROUND_HALF_UP), discount.discount(orderItem));
     }
 
+    @Test
+    public void should_half_off_when_by_four_product() {
+        Discount discount = new GiftWithPurchaseHalfOff();
+        OrderItem orderItem = new OrderItem();
+        Product product = new Product();
+        product.setProductNo("0001");
+        product.setProductName("产品1");
+        product.setPrice(new BigDecimal(100.78));
+        product.getDiscounts().add(discount);
+        Product product1 = new Product();
+        product1.setProductNo("0001");
+        product1.setProductName("产品1");
+        product1.setPrice(new BigDecimal(100.78));
+        product1.getDiscounts().add(discount);
+        Product product2 = new Product();
+        product2.setProductNo("0001");
+        product2.setProductName("产品1");
+        product2.setPrice(new BigDecimal(100.78));
+        product2.getDiscounts().add(discount);
+        Product product3 = new Product();
+        product3.setProductNo("0001");
+        product3.setProductName("产品1");
+        product3.setPrice(new BigDecimal(100.78));
+        product3.getDiscounts().add(discount);
+        orderItem.addProduct(product);
+        orderItem.addProduct(product1);
+        orderItem.addProduct(product2);
+        orderItem.addProduct(product3);
+        assertEquals((new BigDecimal(100.78)).divide(new BigDecimal(2)).setScale(2,  BigDecimal.ROUND_HALF_UP), discount.discount(orderItem));
+    }
+    @Test
+    public void should_not_half_off_when_buy_two_product() {
+        Discount discount = new GiftWithPurchaseHalfOff();
+        OrderItem orderItem = new OrderItem();
+        Product product = new Product();
+        product.setProductNo("0001");
+        product.setProductName("产品1");
+        product.setPrice(new BigDecimal(100.78));
+        product.getDiscounts().add(discount);
+        Product product1 = new Product();
+        product1.setProductNo("0001");
+        product1.setProductName("产品1");
+        product1.setPrice(new BigDecimal(100.78));
+        product1.getDiscounts().add(discount);
+        orderItem.addProduct(product);
+        orderItem.addProduct(product1);
+        assertEquals((new BigDecimal(0)).setScale(2,  BigDecimal.ROUND_HALF_UP), discount.discount(orderItem));
+    }
+
 }
--- src/test/java/com/coding/sales/discount/GiftWithPurchaseOneTest.java
+++ src/test/java/com/coding/sales/discount/GiftWithPurchaseOneTest.java
@@ -42,4 +42,42 @@ public class GiftWithPurchaseOneTest {
 
     }
 
+    @Test
+    public void should_gift_one_when_by_five_product() {
+        Discount discount = new GiftWithPurchaseOne();
+        OrderItem orderItem = new OrderItem();
+        Product product = new Product();
+        product.setProductNo("0001");
+        product.setProductName("产品1");
+        product.setPrice(new BigDecimal(100.78));
+        product.getDiscounts().add(discount);
+        Product product1 = new Product();
+        product1.setProductNo("0001");
+        product1.setProductName("产品1");
+        product1.setPrice(new BigDecimal(100.78));
+        product1.getDiscounts().add(discount);
+        Product product2 = new Product();
+        product2.setProductNo("0001");
+        product2.setProductName("产品1");
+        product2.setPrice(new BigDecimal(100.78));
+        product2.getDiscounts().add(discount);
+        Product product3 = new Product();
+        product3.setProductNo("0001");
+        product3.setProductName("产品1");
+        product3.setPrice(new BigDecimal(100.78));
+        product3.getDiscounts().add(discount);
+        Product product4 = new Product();
+        product4.setProductNo("0001");
+        product4.setProductName("产品1");
+        product4.setPrice(new BigDecimal(100.78));
+        product4.getDiscounts().add(discount);
+        orderItem.addProduct(product);
+        orderItem.addProduct(product1);
+        orderItem.addProduct(product2);
+        orderItem.addProduct(product3);
+        orderItem.addProduct(product4);
+        assertEquals(new BigDecimal(100.78), discount.discount(orderItem));
+
+    }
+
 }

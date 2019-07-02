6db59435285af630ace71eec4f71fb979cf30953
zzh900603 <zhouzh2010@126.com>
2019-07-02 21:28:37 +0800

    满减 实现

---
+++ src/main/java/com/coding/sales/discount/InstantRebate.java
@@ -0,0 +1,40 @@
+package com.coding.sales.discount;
+
+import com.coding.sales.order.OrderItem;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 满减
+ */
+public class InstantRebate extends AbstractDiscont {
+
+    private Map<BigDecimal, BigDecimal> instantRebateMap = new HashMap<BigDecimal, BigDecimal>();
+    {
+        instantRebateMap.put(new BigDecimal(3000.00), new BigDecimal(350.00));
+        instantRebateMap.put(new BigDecimal(2000.00), new BigDecimal(30.00));
+        instantRebateMap.put(new BigDecimal(1000.00), new BigDecimal(10.00));
+    }
+
+
+    @Override
+    public BigDecimal discount(OrderItem orderItem) {
+        if (orderItem == null) {
+            return BigDecimal.ZERO;
+        }
+        BigDecimal subTotal = orderItem.getSubTotal() == null ? BigDecimal.ZERO : orderItem.getSubTotal();
+        BigDecimal instantRebateMoney = BigDecimal.ZERO;
+        BigDecimal temp = BigDecimal.ZERO;
+        for (BigDecimal decimal : instantRebateMap.keySet()) {
+            if (subTotal.compareTo(decimal) > 0) {
+                temp = instantRebateMap.get(decimal);
+                if (temp.compareTo(instantRebateMoney) > 0) {
+                    instantRebateMoney = temp;
+                }
+            }
+        }
+        return instantRebateMoney.setScale(2,  BigDecimal.ROUND_HALF_UP);
+    }
+}

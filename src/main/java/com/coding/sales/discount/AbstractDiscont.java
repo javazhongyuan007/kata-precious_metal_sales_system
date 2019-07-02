8a7b9954103d4e32020222693df92b20f97d4ae9
zzh900603 <zhouzh2010@126.com>
2019-07-02 20:46:38 +0800

    活动折扣接口重构

---
+++ src/main/java/com/coding/sales/discount/AbstractDiscont.java
@@ -0,0 +1,9 @@
+package com.coding.sales.discount;
+
+public abstract class AbstractDiscont implements Discount {
+
+    @Override
+    public DiscountCard getDiscountCard() {
+        return null;
+    }
+}
--- src/main/java/com/coding/sales/discount/Discount.java
+++ src/main/java/com/coding/sales/discount/Discount.java
@@ -1,8 +1,23 @@
 package com.coding.sales.discount;
 
+import com.coding.sales.order.OrderItem;
+import java.math.BigDecimal;
+
 /**
  * 活动
  */
 public interface Discount {
+
+    /**
+     * 折扣
+     * @return 折扣金额
+     */
+    BigDecimal discount(OrderItem orderItem);
+
+    /**
+     * 折扣卡
+     * @return
+     */
+    DiscountCard getDiscountCard();
 
 }

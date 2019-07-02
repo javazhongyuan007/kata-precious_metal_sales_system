45093dbe4da2998891b3adb6c18fa7c64a0eee08
zhmxcn <zhmx1212@163.com>
2019-07-02 19:56:36 +0800

    重构会员类

--- src/main/java/com/coding/sales/member/Member.java
+++ src/main/java/com/coding/sales/member/Member.java
@@ -16,7 +16,62 @@ public class Member {
     private int memberPointsIncreased;
     private int memberPoints;
     private List<DiscountCard> discountCards;
-    private Map<String,Integer> memberTypePoint;
+
+    public String getMemberNo() {
+        return memberNo;
+    }
+
+    public void setMemberNo(String memberNo) {
+        this.memberNo = memberNo;
+    }
+
+    public String getMemberName() {
+        return memberName;
+    }
+
+    public void setMemberName(String memberName) {
+        this.memberName = memberName;
+    }
+
+    public MemberType getOldMemberType() {
+        return oldMemberType;
+    }
+
+    public void setOldMemberType(MemberType oldMemberType) {
+        this.oldMemberType = oldMemberType;
+    }
+
+    public MemberType getNewMemberType() {
+        return newMemberType;
+    }
+
+    public void setNewMemberType(MemberType newMemberType) {
+        this.newMemberType = newMemberType;
+    }
+
+    public int getMemberPointsIncreased() {
+        return memberPointsIncreased;
+    }
+
+    public void setMemberPointsIncreased(int memberPointsIncreased) {
+        this.memberPointsIncreased = memberPointsIncreased;
+    }
+
+    public int getMemberPoints() {
+        return memberPoints;
+    }
+
+    public void setMemberPoints(int memberPoints) {
+        this.memberPoints = memberPoints;
+    }
+
+    public List<DiscountCard> getDiscountCards() {
+        return discountCards;
+    }
+
+    public void setDiscountCards(List<DiscountCard> discountCards) {
+        this.discountCards = discountCards;
+    }
 
     public Member(){
         this.oldMemberType = MemberType.General;
